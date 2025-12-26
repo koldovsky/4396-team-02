54ebcc19e04b73e9079d9601ec3c6bfae0ab8ce0
Wow-ONE <81352298+Hnatyshyn-Volodymyr@users.noreply.github.com>
2025-12-26 15:34:57 +0200

    Create hnatyshyn-volodymyr-solution.js

---
+++ codewars/ SantaClausable Interface/hnatyshyn-volodymyr-solution.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/santaclausable-interface
+function isSantaClausable(obj) {
+  return typeof obj.sayHoHoHo === "function" &&
+         typeof obj.distributeGifts === "function" &&
+         typeof obj.goDownTheChimney === "function";
+}
