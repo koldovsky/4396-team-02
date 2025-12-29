9dcd64ded337a7d3c7eb6400ea799cddad1a8010
Taras Yavorskyy <taras.yavorskyy@i.ua>
2025-12-29 09:09:02 +0200

    added burger menu to about.html

---
+++ scripts/about.js
@@ -0,0 +1,14 @@
+function init() {
+  import("./global.burger-menu.partial.js");
+}
+
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  console.log(`Loaded partials: ${loadedPartialsCount}/${totalPartials}`);
+  if (loadedPartialsCount - 2 === totalPartials) init();
+});
