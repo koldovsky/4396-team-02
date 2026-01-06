d81a7742811089789fa91c2d90c682fcc769f0c8
Wow-ONE <81352298+Hnatyshyn-Volodymyr@users.noreply.github.com>
2026-01-06 13:12:29 +0200

    Update about.our-equipment.partial.js
    Added public init function, fixed import.

--- scripts/about.our-equipment.partial.js
+++ scripts/about.our-equipment.partial.js
@@ -1,21 +1,22 @@
-document.body.addEventListener("htmx:afterSwap", (event) => {
-  if (event.target.classList.contains("our-equipment")) {
-    const images = event.target.querySelectorAll(".our-equipment__grid img");
-    let zoomedImg = null;
+export function initOurEquipment() {
+  const section = document.querySelector(".our-equipment");
+  if (!section) return;
 
-    images.forEach((img) => {
-      img.addEventListener("click", function () {
-        if (zoomedImg === img) {
-          img.classList.remove("img-zoomed");
-          zoomedImg = null;
-        } else {
-          if (zoomedImg) {
-            zoomedImg.classList.remove("img-zoomed");
-          }
-          img.classList.add("img-zoomed");
-          zoomedImg = img;
+  const images = section.querySelectorAll(".our-equipment__grid img");
+  let zoomedImg = null;
+
+  images.forEach((img) => {
+    img.addEventListener("click", () => {
+      if (zoomedImg === img) {
+        img.classList.remove("img-zoomed");
+        zoomedImg = null;
+      } else {
+        if (zoomedImg) {
+          zoomedImg.classList.remove("img-zoomed");
         }
-      });
+        img.classList.add("img-zoomed");
+        zoomedImg = img;
+      }
     });
-  }
-});
+  });
+}
